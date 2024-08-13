c86adc9eb80431f47dd6dd8c1a79f0425562af3f
Erik Kvietelaitis <erikkvietelaitis@gmail.com>
2024-08-13 12:59:00 +1000

    commenting for easy of understanding

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use crate::LaraCore::CoreEnums::*;
 use crate::LaraCore::CoreStruts::*;
 pub mod LaraCore;
 
-
+// Declare the linux_bridge module
 mod linux_bridge; 
 use crate::linux_bridge::linux::system_time;
 
 fn main(){
        println!("Welcome to L.A.R.A.!");
-       system_time();
+       system_time();   // Call the system_time function from the linux module
 }
 
 
