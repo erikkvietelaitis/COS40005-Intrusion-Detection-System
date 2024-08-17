8f30492f7f5f5dee145b9e9e31d99196dabf1040
DoctorLock <103989173@student.swin.edu.au>
2024-08-18 09:51:17 +1000

    Added example module

--- src/LaraCore/CoreTraits.rs
+++ src/LaraCore/CoreTraits.rs
@@ -1,8 +1,17 @@
-
 use crate::LaraCore::CoreStruts::*;
 use std::time::*;
 
-pub trait AnalysisModule{
-    fn perform_analysis(&self) -> Vec<Log>;
-    fn new() -> Self;
+pub trait AnalysisModule {
+    /**
+     * Use this function to gather data from linux systems. Return true unless error occurred
+     */
+    fn get_data(&mut self) -> bool;
+    /**
+     * generate testing data. This function should NOT interact with linux, merely just generate fake data.
+     */
+    fn get_testing_data(&mut self) -> bool;
+    /**
+     * This analyses the most recent set of data and returns logs to be logged.
+     */
+    fn perform_analysis(&mut self) -> Vec<Log>;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
+use std::vec;
+
+use LaraCore::CoreStruts;
+use LaraCore::CoreTraits;
+use LaraCore::CoreTraits::AnalysisModule;
+
+use crate::AnalysisModules::*;
 use crate::LaraCore::CoreEnums::*;
 use crate::LaraCore::CoreStruts::*;
+pub mod AnalysisModules;
 pub mod LaraCore;
 
 // test from Ben  from his branch
 
 fn main() {
-    println!("Welcome to L.A.R.A.!");
+    println!("Welcome to the IDS software");
+    //having issues defining an list of all analysis modules
+    let mut e_module = AnalysisModules::Example::Example::default();
+    // let mut v: [dyn CoreTraits::AnalysisModule;1] = [];
+    let mut logs: Vec<Log> = Vec::new();
+    let mut i = 0;
+    // let mut l = .into_iter();
+    while i < 10 {
+        e_module.get_data();
+        logs = e_module.perform_analysis();
+        // Add other modules here
+        for l in logs {
+            println!("{}", l.build_alert());
+        }
+        i = i + 1;
+    }
 }
