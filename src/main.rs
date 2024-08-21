be9b9c1cedd0a1d6cad27a967860c2976ec66aed
DoctorLock <103989173@student.swin.edu.au>
2024-08-21 23:10:10 +1000

    working copy of code

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::vec;
 use LaraCore::CoreStruts;
 use LaraCore::CoreTraits;
 use LaraCore::CoreTraits::AnalysisModule;
+use std::{thread, time};
 
 use crate::AnalysisModules::*;
 use crate::LaraCore::CoreEnums::*;
@@ -10,23 +11,32 @@ use crate::LaraCore::CoreStruts::*;
 pub mod AnalysisModules;
 pub mod LaraCore;
 
-// test from Ben  from his branch
 
 fn main() {
-    println!("Welcome to the IDS software");
+    println!("Chromia loading");
+    // Should be loaded by configuration. Higher number means lower performance impact of the IDS
+    let tick_intervals = time::Duration::from_millis(1000);    
+    println!("tick interval set to {} milliseconds", tick_intervals.as_millis());
     //having issues defining an list of all analysis modules
-    let mut e_module = AnalysisModules::Example::Example::default();
-    // let mut v: [dyn CoreTraits::AnalysisModule;1] = [];
+    let mut modules: Vec<Box<dyn AnalysisModule>>;
+    modules = vec![Box::new(
+        <AnalysisModules::Example::Example as std::default::Default>::default(),
+    )];
+    println!("Successfully built ");
     let mut logs: Vec<Log> = Vec::new();
     let mut i = 0;
-    // let mut l = .into_iter();
-    while i < 10 {
-        e_module.get_data();
-        logs = e_module.perform_analysis();
-        // Add other modules here
-        for l in logs {
-            println!("{}", l.build_alert());
-        }
-        i = i + 1;
-    }
+    // loop{
+        
+
+    //     thread::sleep(tick_intervals);
+    // }
+    // while i < 10 {
+    //     e_module.get_data();
+    //     logs = e_module.perform_analysis();
+    //     // Add other modules here
+    //     for l in logs {
+    //         println!("{}", l.build_alert());
+    //     }
+    //     i = i + 1;
+    // }
 }
