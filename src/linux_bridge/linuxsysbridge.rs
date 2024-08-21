a25f1ac1696d492aebf6fbf38b7c20f9276c3af5
DoctorLock <103989173@student.swin.edu.au>
2024-08-21 11:52:15 +1000

    Updated formatting

--- src/linux_bridge/linuxsysbridge.rs
+++ src/linux_bridge/linuxsysbridge.rs
@@ -23,24 +23,24 @@ pub fn system_time() {
 }
 // Function to get and print system name "Ubuntu"
 pub fn system_name(){
-let system_name: String = System::name().unwrap();
-println!("{}",system_name);
+    let system_name: String = System::name().unwrap();
+    println!("{}",system_name);
 }
 // Function to get and print system host name "eriklaptop"
 pub fn system_host_name(){
-let system_host_name: String = System::host_name().unwrap();
-println!("{}",system_host_name);
+    let system_host_name: String = System::host_name().unwrap();
+    println!("{}",system_host_name);
 }
 // Function to get and print system kernel version "5.4.0-42-generic"
 pub fn system_kernel_version(){
-let system_kernel: String = System::kernel_version().unwrap();
-print!("{}",system_kernel);
+    let system_kernel: String = System::kernel_version().unwrap();
+    print!("{}",system_kernel);
 }
 // Function to get and print system OS version "20.04.1"
 pub fn system_os_version(){
     let system_os_version: String = System::os_version().unwrap();
-   print!("{}", system_os_version);
-    }
+    print!("{}", system_os_version);
+}
 /* 
 Function to read the content of a file, with a path specified as a parameter, and return the content as a string (content), hence returns an io::Result<String> type.
 The file information is stored in the buffer, and the content is read line by line and stored in the content variable.
@@ -62,3 +62,5 @@ pub fn sys_file_read(filepath: &str) -> io::Result<String> {
     }
      Ok(content)
 }
+
+
