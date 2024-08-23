19fa1612d4f5e7026b448b18c2555d4e9d718f11
brokenpip <161807146+brokenpip@users.noreply.github.com>
2024-08-23 18:05:17 +1000

    still learning

--- hcm/src/main.rs
+++ hcm/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println(hash)
+}
+
+fn hashthis() {
+    let mut temp_file = HashThisFile::new()?;
+    io::copy(&mut input, &mut temp_file)?;
+    temp_file.flush()?;
+    let temp_path = temp_file.path().to_str()
 }
