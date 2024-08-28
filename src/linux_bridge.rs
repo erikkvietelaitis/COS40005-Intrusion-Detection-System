d22d507263a2b492b5c772add9203d3f69ff50ae
stackingheaps <sean-wilson_@hotmail.com>
2024-08-28 13:05:50 +1000

    tested and working port scanner
    port_scanner utilised please see code and test

--- src/linux_bridge.rs
+++
@@ -1,3 +0,0 @@
-pub mod auth;
-pub mod network;
-pub mod system;
--- src/linux_bridge/network.rs
+++
@@ -1,7 +0,0 @@
-use pnet::datalink as pnet_datalink;
-
-pub fn ipadd() {
-    for interface in pnet_datalink::interfaces() {
-        println!("{}", interface);
-    }
-}
