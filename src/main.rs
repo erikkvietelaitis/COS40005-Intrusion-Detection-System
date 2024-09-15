588c752347e37333f5cf1600b9cfd88833f79f6a
DoctorLock <103989173@student.swin.edu.au>
2024-09-15 16:52:14 +1000

    update

--- src/main.rs
+++ src/main.rs
@@ -127,8 +127,10 @@ fn append_to_log(message: &str) -> std::io::Result<()> {
         .write(true)
         .append(true)
         .create(true)  // This will create the file if it doesn't exist
-        .open("Chormia.log")?;
+        .open("Chromia.log").unwrap();
+    if let Err(e) = writeln!(file, "A new line!") {
+        println!("Couldn't write to file: {}", e);
+    }
 
-    writeln!(file, "{}", message)?;  // Write the message and append a newline
     Ok(())
 }
