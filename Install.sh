1f5d4e183b877a5bb6a196d311438c9ea40cbf45
Lachlan Kearney <74101712+DoctorLock@users.noreply.github.com>
2024-09-16 10:45:08 +1000

    updated install script so it can be run anywhere, and has extra pizazz

--- Install.sh
+++ Install.sh
@@ -1,8 +1,46 @@
+	#!/bin/bash
+CHROMIA_PAT="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"
+# Define color codes
 #!/bin/bash
-CHROMIA_PAT="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"
 
+# Define color codes
+COLOR_RESET="\033[0m"
+COLOR_BLUE="\033[34m"
+COLOR_GREEN="\033[32m"
+COLOR_CYAN="\033[36m"
+COLOR_RED="\033[31m"
+TEXT_BOLD="\033[1m"
+# Clear the terminal
+clear
 
+# Display ASCII art for Chromia with each line in a different color
+echo -e "${COLOR_BLUE}"
+echo -e " _______           _______  _______  _______ _________ _______ 
+(  ____ \|\     /|(  ____ )(  ___  )(       )\__   __/(  ___  ) ${COLOR_GREEN}"
 
+echo -e "| (    \/| )   ( || (    )|| (   ) || () () |   ) (   | (   ) |
+| |      | (___) || (____)|| |   | || || || |   | |   | (___) |${COLOR_CYAN}"
+
+echo -e "| |      |  ___  ||     __)| |   | || |(_)| |   | |   |  ___  |
+| |      | (   ) || (\ (   | |   | || |   | |   | |   | (   ) |    ${COLOR_RED}"
+
+echo "| (____/\| )   ( || ) \ \__| (___) || )   ( |___) (___| )   ( |
+(_______/|/     \||/   \__/(_______)|/     \|\_______/|/     \|"
+
+echo -e "${COLOR_GREEN}"
+
+cat << EOF
+
+Chromia IDS Software
+Version: 1.0.0
+Authors: Jane Doe, John Smith
+
+Chromia is an Intrusion Detection System designed to protect your network from malicious activity.
+It provides real-time monitoring and alerts to help you maintain security.
+
+EOF
+echo -e "${COLOR_RED} ${TEXT_BOLD} Chromia must be run as sudo, it may not function properly without this!"
+echo -e "${COLOR_RESET}"
 # Update and Upgrade the System
 echo "Updating and upgrading system..."
 sudo apt-get update && apt-get upgrade -y
@@ -13,14 +51,7 @@ PASSWORD="Password12345:)"
 USER_HOME="/home/$USER_NAME"
 GROUP_NAME="Chromia_Group"
 
-    echo "Creating user $USER_NAME..."
-    sudo groupadd $GROUP_NAME
-    sudo useradd -m -g $GROUP_NAME $USER_NAME
-    echo $USER_NAME:$PASSWORD | sudo chpasswd
-    echo "Allowing $USER_NAME access to /var/log/btmp without sudo..."
-    sudo usermod -aG $GROUP_NAME $USER_NAME
-    sudo chown root:$GROUP_NAME /var/log/btmp
-    sudo chmod 0640 /var/log/btmp
+
 
 
 
@@ -50,14 +81,14 @@ echo "Installing git..."
 sudo apt install -y git
 
 #git clone Chromia
-git clone https://erikkvietelaitis:$CHROMIA_PAT@github.com/erikkvietelaitis/COS40005-Intrusion-Detection-System.git -v
+git clone https://erikkvietelaitis:$CHROMIA_PAT@github.com/erikkvietelaitis/COS40005-Intrusion-Detection-System.git
 
 #build Chromia
-cd $HOME/Downloads/COS40005-Intrusion-Detection-System
+cd ./COS40005-Intrusion-Detection-System
 cargo build --release
 
 #move Chromia build to /bin
-cd $HOME/Downloads/COS40005-Intrusion-Detection-System/target/release
+cd ./target/release
 mkdir -p ~/bin
 mv Chromia ~/bin/
 
@@ -65,4 +96,7 @@ mv Chromia ~/bin/
 #cargo run --release
 
 #remove Chromia files
-rm -rf ~/Downloads/COS40005-Intrusion-Detection-System
+ls
+cd ../../../
+ls
+rm -rf ./COS40005-Intrusion-Detection-System
