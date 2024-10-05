8efbddb106182c568227d96965b8dadbb7a2c528
brokenpip <161807146+brokenpip@users.noreply.github.com>
2024-10-05 13:52:31 +1000

    missing functions added

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ pub mod linux_bridge;
 // for tpm tie-in
 use std::process::{self};
 use core::time;
-use std::process::{self};
 use std::process::{Command,Stdio};
 use std::str;
 use std::io;
@@ -296,4 +295,58 @@ fn append_to_log(message: &str, log_dir: &Path) -> std::io::Result<()> {
 
     writeln!(file, "{}", message)?; // Write the message and append a newline
     Ok(())
+}
+fn lock_check(target_pid: &u32) -> (bool, u32) {
+    let lock_name = directory_read("/var/chromia/ids").unwrap_or_else(|| "aa".to_string());
+    let lock_pid: u32 = lock_name.parse().unwrap_or(0);
+    if lock_pid == 0 {
+        return (true, 0);
+    } else if lock_pid == *target_pid {
+        return (true, *target_pid);
+    } else {
+        return (false, lock_pid);
+    }
+}
+fn genhash(key: &str) -> (bool, String) {
+    let output = match Command::new("/bin/Chromia/Data/b3sum")
+        .arg(key)
+        .arg("--no-names")
+        .output() {
+        
+        Ok(output) => output,
+        Err(err) => {
+            append_to_log(&format!("Failed to execute command for key '{}': {}", key, err));
+            return (false, String::new());
+        }
+    };
+    // Convert output to string
+    let stdout_str = String::from_utf8_lossy(&output.stdout).into_owned();
+    let stderr_str = String::from_utf8_lossy(&output.stderr).into_owned();
+    
+    //println!("{}", stdout_str);
+
+    if !stderr_str.is_empty() {
+        append_to_log(&format!("stderr for key '{}': {}", key, stderr_str));
+    }
+
+    (true, stdout_str)
+}
+fn directory_read(path: &str) -> Option<String> {
+    let entries = fs::read_dir(path).ok()?;
+
+    if let Some(entry) = entries.into_iter().next() {
+        let entry = entry.ok()?;
+        let path = entry.path();
+
+        if path.is_file() {
+            if let Some(name_str) = path.file_name().and_then(|name| name.to_str()) {
+                return Some(name_str.to_string());
+            }
+        }
+    }
+
+    None
+}
+fn file_check(path: &str) -> bool {
+    Path::new(path).exists()
 }
